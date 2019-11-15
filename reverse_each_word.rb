9d2d5ad2dcc3ae37f8cb746b46d18b7f4c6a0153
Sean Tarzy <seantarzy@gmail.com>
2019-11-15 17:15:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,9 @@
-def reverse_each_word(string)
-  new_array = []
-  sentence = string.split(" ")
-  sentence.each do |word|
-new_array << word.reverse 
-end 
-return new_array.join(" ")
-end 
 
-# def reverse_each_word(string)
-#   sentence = string.split(" ")
-#   sentence.collect do |word|
-# word.reverse
-# end 
-# .join
-# end 
+
+def reverse_each_word(string)
+  sentence = string.split(" ")
+  sentence.collect do |word|
+word.reverse
+end 
+.join
+end 
